5cee20fe4fe3d647df3c310438b1b18728c52284
kanetaku1 <ublvsp010111@gmail.com>
2024-04-25 12:43:25 +0900

    データベースからクイズを取得するクラスを作成(途中)

---
+++ src/main/java/DBconnet/getQuiz.java
@@ -0,0 +1,58 @@
+package DBconnet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class getQuiz {
+
+  public String imagePath;
+  public String genre;
+  public String question;
+  public String answer;
+  
+  public void GetData(){
+    try {
+      // PHPファイルのURL
+      URL url = new URL("http://localhost/minhaya/quiz_get.php");
+      // HTTPリクエストの作成
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+      // レスポンスを取得
+      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      String inputLine;
+      StringBuilder response = new StringBuilder();
+      while ((inputLine = in.readLine()) != null) {
+          response.append(inputLine);
+      }
+      in.close();
+
+      //JSON解析
+      JsonAnalyzer(response.toString());
+
+    } catch (IOException e){
+        e.printStackTrace();
+    } 
+  }
+
+  private void JsonAnalyzer(String response){
+    JSONArray jsonArray = new JSONArray(response);
+    for (int i = 0; i < jsonArray.length(); i++){
+      JSONObject jsonObject = jsonArray.getJSONObject(i);
+      this.imagePath = jsonObject.getString("image_path");
+      this.genre = jsonObject.getString("genre");
+      this.question = jsonObject.getString("question");
+      this.answer = jsonObject.getString("answer");
+
+      // ここで取得したデータを利用して何かを行う
+      System.out.println("Image Path: " + imagePath);
+      System.out.println("Genre: " + genre);
+      System.out.println("Question: " + question);
+      System.out.println("Answer: " + answer);
+    }
+  }
+}
--- src/main/java/minhaya/WebSocketServer.java
+++ src/main/java/minhaya/WebSocketServer.java
@@ -2,18 +2,69 @@ package minhaya;
 
 import org.glassfish.tyrus.server.Server;
 
+import DBconnet.getQuiz;
+
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @ServerEndpoint("/websocket")
 public class WebSocketServer {
 
+    private List<String> questions = new ArrayList<>();
+    private int currentQuestionIndex = -1;
+
+    public WebSocketServer() {
+        // データベースから問題を取得するメソッドを呼び出して、questionsリストに問題を格納する
+        getQuiz quiz = new getQuiz();
+        quiz.GetData();
+        questions.add(quiz.question);
+        // ここではダミーデータを追加しています
+        // questions.add("問題1: 1 + 1 は？");
+        // questions.add("問題2: 東京の首都は？");
+        // 他の問題も追加する...
+    }
+    
     @OnMessage
     public void onMessage(String message, Session session) throws IOException {
         System.out.println("Received message: " + message);
-        session.getBasicRemote().sendText("Server received your message: " + message);
+        // session.getBasicRemote().sendText("Server received your message: " + message);
+        if (message.equals("request_question")) {
+            sendNextQuestion(session);
+        } else {
+            // クライアントからの回答を受け取り、正誤判定を行うメソッドを呼び出す
+            String answer = message;
+            checkAnswerAndSendResult(answer, session);
+        }
+    }
+
+    private void sendNextQuestion(Session session) throws IOException {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.size()) {
+            String question = questions.get(currentQuestionIndex);
+            session.getBasicRemote().sendText(question);
+        } else {
+            session.getBasicRemote().sendText("問題はこれで終了です。");
+            // ここで必要な後処理を行う（例：最終結果を表示するなど）
+        }
+    }
+
+    private void checkAnswerAndSendResult(String answer, Session session) throws IOException {
+        // 回答を正誤判定する処理を実装し、結果をクライアントに送信する
+        // ここでは単純化して、正解は"2"とする
+        String result;
+        if (answer.equals("2")) {
+            result = "正解です！次の問題を送信します。";
+        } else {
+            result = "不正解です。";
+        }
+        session.getBasicRemote().sendText(result);
+        if (!result.equals("不正解です。")) {
+            sendNextQuestion(session);
+        }
     }
 
     public void StartServer() {
